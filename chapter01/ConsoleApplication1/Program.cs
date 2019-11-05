db83cfbb37ecbf789a03f33920d80b6cc076c6a4
Guan Chao <levid.gc@outlook.com>
2019-11-05 15:20:09 +0800

    update

---
+++ chapter01/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SimpleParserTests.TestReturnZeroWhenEmptyString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ chapter01/ConsoleApplication1/SimpleParserTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApplication1
+{
+    public class SimpleParserTests
+    {
+        public static void TestReturnZeroWhenEmptyString()
+        {
+            string testName = MethodBase.GetCurrentMethod().Name;
+            try
+            {
+                SimpleParser p = new SimpleParser();
+                int result = p.ParseAndSum(string.Empty);
+                if (result != 0)
+                {
+                    TestUtil.ShowProblem(testName, " Parse and sum should have returned 0 on an empty string");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
